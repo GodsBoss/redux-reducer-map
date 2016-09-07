db2df5346daae4e065c175d5762419fa03b00d78
Timo Reitz <godsboss@gmx.de>
2016-09-07 21:25:26 +0200

    Enhance test readibility.

--- test/redux-reducer-map-test.js
+++ test/redux-reducer-map-test.js
@@ -3,25 +3,28 @@ import expect from 'expect.js'
 
 describe('Reducer created via Redux Reducer Map', () => {
 
+  const EMPTY_STATE = {}
+
   it('returns the initial state if given none', () => {
 
     const initialState = { foo: 'bar' }
-    const reducedState = createReducerViaMap({}, initialState)(undefined, { type: 'NOP' })
+    const reducerMap = {}
+    const action = { type: 'NOP' }
+    const reducedState = createReducerViaMap(reducerMap, initialState)(undefined, action)
 
     expect(reducedState).to.eql(initialState)
   })
 
   it('passes state and action to the appropriate function of the map', () => {
 
-    const reduce = createReducerViaMap(
-      {
-        USED: (state, action) => ({ reducer: 'USED', action: action, state: state }),
-        UNUSED: (state, action) => ({ reducer: 'UNUSED'})
-      },
-      {}
-    )
-
-    const reducedState = reduce({ value: 1500 }, { type: 'USED', value: 2500})
+    const reducerMap = {
+      USED: (state, action) => ({ reducer: 'USED', action: action, state: state }),
+      UNUSED: (state, action) => ({ reducer: 'UNUSED'})
+    }
+    const reduce = createReducerViaMap(reducerMap, EMPTY_STATE)
+    const state = { value: 1500 }
+    const action = { type: 'USED', value: 2500 }
+    const reducedState = reduce(state, action)
 
     expect(reducedState.reducer).to.be('USED')
     expect(reducedState.state.value).to.be(1500)
@@ -31,7 +34,9 @@ describe('Reducer created via Redux Reducer Map', () => {
   it('throws an error by default if action type is not found', () => {
     const UNKNOWN_ACTION_TYPE = 'UNKNOWN'
     const usingUnknownActionType = () => {
-      createReducerViaMap({}, {})({}, { type: UNKNOWN_ACTION_TYPE })
+      const reducerMap = {}
+      const action = { type: UNKNOWN_ACTION_TYPE }
+      createReducerViaMap(reducerMap, EMPTY_STATE)(EMPTY_STATE, action)
     }
     expect(usingUnknownActionType).to.throwException((e) => {
       expect(e.message).to.contain(UNKNOWN_ACTION_TYPE)
